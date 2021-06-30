a1ae6217d95cd704c5e353025a0b2832584214c1
lohithsrk <srklohith05@gmail.com>
2021-06-30 19:49:27 +0530

    add sign in and sign up functionality

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.component";
 import { auth } from "./firebase/firebase.utils";
+import { createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends Component {
   constructor() {
@@ -16,16 +17,28 @@ class App extends Component {
     };
   }
 
-  unsubscribeFromAuth = null
+  unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user})
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
+    });
   }
 
   componentWillUnmount() {
-    this.unsubscribeFromAuth()
+    this.unsubscribeFromAuth();
   }
 
   render() {
--- src/pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.component.jsx
+++ src/pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './sign-in-and-sign-up-page.styles.scss'
 import SignIn from '../../components/sign-in/sign-in.components';
+import SignUp from '../../components/sign-up/sign-up.components';
 
 const SignInAndSignUpPage = () => (
     <div className="sign-in-and-sign-up-page">
         <SignIn />
+        <SignUp />
     </div>
 )
 
