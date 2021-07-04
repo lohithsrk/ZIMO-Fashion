b7eeb00ae132c37a3e9eb825944dce261e5e688d
lohithsrk <srklohith05@gmail.com>
2021-07-04 14:02:59 +0530

    add persist-redux

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import {Provider} from 'react-redux'
-import store from './redux/store'
+import {persistGrate} from 'redux-persist/integration/react'
+import {store, presistor} from './redux/store'
 
 ReactDOM.render(
   <Provider store={store}>
   <BrowserRouter>
+  <persistGrate presistor={presistor}>
   <App />
+  </persistGrate>
   </BrowserRouter>
   </Provider>,
   document.getElementById('root')
