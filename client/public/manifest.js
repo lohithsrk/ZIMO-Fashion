8f7f7f454009813ab79ee16529d4677ffda41765
lohithsrk <srklohith05@gmail.com>
2021-07-21 20:44:44 +0530

    Changes

--- client/public/manifest.js
+++ client/public/manifest.js
@@ -1,11 +1,15 @@
 {
     "short_name": "ZIMO",
     "name": "Zimo-Fashion",
-    "icons": {
-        "src": "favicon.ico",
+    "icons": [{
+        "src": "Zimo.png",
         "sizes": "64x64 32x32 24x24 16x16",
-        "type": "image/x-icon"
-    },
+        "type": "image/png"
+    }, {
+        "src": "Zimo.png",
+        "sizes": "64x64 32x32 24x24 16x16",
+        "type": "image/png"
+    }],
     "start_url": ".",
     "display": "standalone",
     "theme_color": "#000000",
