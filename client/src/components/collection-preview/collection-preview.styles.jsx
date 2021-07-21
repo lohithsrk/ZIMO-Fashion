514df87c4c3a4600fb889ed14db2bf0932cf1915
lohithsrk <srklohith05@gmail.com>
2021-07-21 19:58:46 +0530

    Added PWA

--- client/src/components/collection-preview/collection-preview.styles.jsx
+++ client/src/components/collection-preview/collection-preview.styles.jsx
@@ -4,7 +4,6 @@ export const CollectionPreviewContainer = styled.div`
   display: flex;
   flex-direction: column;
   margin-bottom: 30px;
-
   @media screen and (max-width: 800px) {
     align-items: center;
   }
@@ -14,16 +13,16 @@ export const TitleContainer = styled.h1`
   font-size: 28px;
   margin-bottom: 25px;
   cursor: pointer;
-
   &:hover {
     color: grey;
   }
 `;
 
+TitleContainer.displayName = 'TitleContainer';
+
 export const PreviewContainer = styled.div`
   display: flex;
   justify-content: space-between;
-
   @media screen and (max-width: 800px) {
     display: grid;
     grid-template-columns: 1fr 1fr;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+const enforce = require('express-sslify');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -11,6 +13,8 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
+app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, 'client/build')));
@@ -19,6 +23,15 @@ if (process.env.NODE_ENV === 'production') {
 		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 	});
 }
+
+app.listen(port, (error) => {
+	if (error) throw error;
+	console.log('Server running on port ' + port);
+});
+
+app.get('/service-worker.js', (req, res) => {
+	res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+});
 
 app.post('/payment', (req, res) => {
 	const body = {
@@ -34,9 +47,4 @@ app.post('/payment', (req, res) => {
 			res.status(200).send({ success: stripeRes });
 		}
 	});
-});
-
-app.listen(port, (error) => {
-	if (error) throw error;
-	console.log('Server running on port ' + port);
 });
