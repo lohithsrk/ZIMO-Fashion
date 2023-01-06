37681e96b954908259a44a96ad71ba1750dbbb31
lohithsrk <srklohith05@gmail.com>
2023-01-06 09:19:04 +0530

    changes

---
+++ client/build/precache-manifest.d306e43fa718185131a72d6abfd6db9a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f316a7f9eb49a378442",
+    "url": "/static/css/main.cec14241.chunk.css"
+  },
+  {
+    "revision": "6f316a7f9eb49a378442",
+    "url": "/static/js/main.6f316a7f.chunk.js"
+  },
+  {
+    "revision": "d8af9d828aaac0d0d63f",
+    "url": "/static/js/1.d8af9d82.chunk.js"
+  },
+  {
+    "revision": "c5ae15eec955631067d8",
+    "url": "/static/js/2.c5ae15ee.chunk.js"
+  },
+  {
+    "revision": "1b0fde6b329b78991690",
+    "url": "/static/js/3.1b0fde6b.chunk.js"
+  },
+  {
+    "revision": "022490ae4f85516b44fb",
+    "url": "/static/js/4.022490ae.chunk.js"
+  },
+  {
+    "revision": "b2e8924fd59146dcfc0e",
+    "url": "/static/js/5.b2e8924f.chunk.js"
+  },
+  {
+    "revision": "c570a6c1b165b5dd1444",
+    "url": "/static/js/6.c570a6c1.chunk.js"
+  },
+  {
+    "revision": "dbd7a8b1cd0333d70a54",
+    "url": "/static/js/7.dbd7a8b1.chunk.js"
+  },
+  {
+    "revision": "862f7fbba1781f5b95ac",
+    "url": "/static/js/8.862f7fbb.chunk.js"
+  },
+  {
+    "revision": "5c3e3235cbbf5055f9d0",
+    "url": "/static/js/runtime~main.5c3e3235.js"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  },
+  {
+    "revision": "669c6ab057bcd5064201ac5eaea22053",
+    "url": "/index.html"
+  }
+];
